165070138d10d237f5a85623b39bc3493ca28d97
Pratik <bopsdlops>
2025-06-17 15:26:03 +0545

    install latest schedule and defined ccrypto

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as crypto from 'node:crypto';
+
+(global as any).crypto = crypto;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
